b4bc706e232f9a7930dc1f0d158a58e4e7bfce59
Dan Barragan <danbarragan@EZBAKE.localdomain>
2020-05-02 12:32:26 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "drawClient.html", nil)
+	})
+	router.Run(":" + port)
+}
