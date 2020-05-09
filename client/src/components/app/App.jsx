cc0fb4c97bdc4e1525e44b895caf619444a7620e
Dan Barragan <danbarragan@gmail.com>
2020-05-08 17:17:38 -0700

    Client-side group selection

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Login from '../login/Login'
+import Login from '../login/Login';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Login />
     </div>
   );
 }
