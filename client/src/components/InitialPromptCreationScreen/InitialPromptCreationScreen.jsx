c7de9e46a36b4c6c1e197849d651a8d4e89b75f1
Pericles Tsellos <pericles.tsellos@gmail.com>
2020-05-20 19:55:21 -0700

    Added a bit to the client side prompt creation (#43)
    * Added a bit to the client side prompt creation
    
    * added the waiting for players screen element to match what was done for drawing screen
    
    * fixed a few things on promptCreation client

--- client/src/components/InitialPromptCreationScreen/InitialPromptCreationScreen.jsx
+++ client/src/components/InitialPromptCreationScreen/InitialPromptCreationScreen.jsx
@@ -1,26 +1,63 @@
 import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import './InitialPromptCreationScreen.css';
 import { formatServerError } from '../../utils/errorFormatting';
+import UpdateGameState from '../../utils/updateGameState';
 
 
 class InitialPromptCreationScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      noun: '',
+      adjective1: '',
+      adjective2: '',
       error: null,
     };
+
+    this.onSubmitPromptButtonClicked = this.onSubmitPromptButtonClicked.bind(this);
     this.updateGameState = this.updateGameState.bind(this);
+    this.onNounChange = this.onNounChange.bind(this);
+    this.onAdjective1Change = this.onAdjective1Change.bind(this);
+    this.onAdjective2Change = this.onAdjective2Change.bind(this);
   }
 
-  async onStartGameButtonClicked() {
+
+  componentWillUnmount() {
+    const { timerId } = this.state;
+    if (timerId !== null) {
+      clearInterval(timerId);
+    }
+  }
+
+  onNounChange(event) {
+    this.setState({ noun: event.target.value });
+  }
+
+  onAdjective1Change(event) {
+    this.setState({ adjective1: event.target.value });
+  }
+
+  onAdjective2Change(event) {
+    this.setState({ adjective2: event.target.value });
+  }
+
+  async onSubmitPromptButtonClicked() {
     const { gameState, onGameStateChanged } = this.props;
     const { groupName, currentPlayer } = gameState;
     const { name } = currentPlayer;
-    const data = { playerName: name, groupName };
+    const { noun, adjective1, adjective2 } = this.state;
+    const data = {
+      playerName: name, groupName, noun, adjective1, adjective2,
+    };
+
     try {
-      const response = await axios.post('/api/start-game', data);
+      const response = await axios.post('/api/add-prompt', data);
       onGameStateChanged(response.data);
+      // Start listening for game state updates
+      const timerId = setInterval(this.updateGameState, 3000);
+      this.setState({ timerId });
     } catch (error) {
       this.setState({ error: formatServerError(error) });
     }
@@ -31,19 +68,59 @@ class InitialPromptCreationScreen extends React.Component {
     const { gameState, onGameStateChanged } = this.props;
     const { groupName, currentPlayer } = gameState;
     const { name: playerName } = currentPlayer;
-    try {
-      const response = await axios.get(`/api/get-game-status/${groupName}?playerName=${playerName}`);
-      onGameStateChanged(response.data);
-    } catch (error) {
-      this.setState({ error: formatServerError(error) });
-    }
+    UpdateGameState(
+      groupName,
+      playerName,
+      onGameStateChanged,
+      (error) => { this.setState({ error: formatServerError(error) }); },
+    );
   }
 
   render() {
-    const { error } = this.state;
+    const {
+      noun, adjective1, adjective2, error,
+    } = this.state;
+    const { gameState } = this.props;
+    const { players, currentPlayer } = gameState;
+
+
+    const promptCreatingElements = (
+      <div>
+        <h3>Enter the prompt for other players to draw</h3>
+        <label htmlFor="adjective1">
+          First Adjective
+          <input id="adj1" type="text" value={adjective1} onChange={this.onAdjective1Change} />
+        </label>
+        <label htmlFor="adjective2">
+          Second Adjective
+          <input id="adj2" type="text" value={adjective2} onChange={this.onAdjective2Change} />
+        </label>
+        <label htmlFor="noun">
+          Noun
+          <input id="noun" type="text" value={noun} onChange={this.onNounChange} />
+        </label>
+        <button className="button buttonTypeA" type="button" onClick={this.onSubmitPromptButtonClicked}>Submit Prompt</button>
+      </div>
+    );
+
+    const filteredPlayers = players.filter((player) => player.hasPendingAction === true);
+    const playersPendingAction = filteredPlayers.map((player) => (
+      <li key={player.name}>
+        {player.name}
+      </li>
+    ));
+
+    const waitingElements = (
+      <div>
+        <h3 className="initialPromptCreationScreen">Thanks, waiting for these players to finish submitting their prompts:</h3>
+        <ul>{playersPendingAction}</ul>
+      </div>
+    );
+
+
     return (
-      <div className="InitialPromptCreationScreen">
-        Adjective, adjective, noun.
+      <div className="screen initialPromptCreationScreen">
+        {currentPlayer.hasCompletedAction ? waitingElements : promptCreatingElements}
         <h3 className="error">{error}</h3>
       </div>
     );
@@ -55,6 +132,7 @@ InitialPromptCreationScreen.propTypes = {
     currentPlayer: PropTypes.shape({
       name: PropTypes.string.isRequired,
       isHost: PropTypes.bool.isRequired,
+      hasCompletedAction: PropTypes.bool.isRequired,
     }).isRequired,
     players: PropTypes.arrayOf(PropTypes.shape({
       name: PropTypes.string.isRequired,
@@ -63,5 +141,6 @@ InitialPromptCreationScreen.propTypes = {
   }).isRequired,
   onGameStateChanged: PropTypes.func.isRequired,
 };
+
 
 export default InitialPromptCreationScreen;
